61ef5a30921793d8f90138161f4eea1e248be737
cady2015 <604548568@qq.com>
2018-11-07 12:58:37 +0800

    1.增加文件下载工具以及测试用例

---
+++ datasynch_provider/src/test/java/com/cetc/cloud/test/ImageDownLoader.java
@@ -0,0 +1,68 @@
+package com.cetc.cloud.test;
+
+import com.cetc.cloud.datasynch.provider.core.util.FileDownloadUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class ImageDownLoader {
+
+    Logger logger = LoggerFactory.getLogger(ImageDownLoader.class);
+    @Autowired
+    @Qualifier("readOnlyJdbcTemplate")
+    JdbcTemplate readOnlyJdbcTemplate;
+
+    @Test
+    public void testDownloadImages() throws Exception {
+        //读取列表
+        String SQL = "SELECT DISTINCT filepath FROM \"QXFJ_BUILD_IMAGE_V\" \n" +
+                "WHERE filepath IS NOT NULL\n" +
+                "AND filepath NOT IN (SELECT filepath FROM \"QXFJ_BUILD_IMAGE_V\" \n" +
+                "WHERE filepath ='undefined' )";
+        String rootpath = "D:\\build_image_v";
+        SqlRowSet sqlRowSet = readOnlyJdbcTemplate.queryForRowSet(SQL);
+        ArrayList<String> filePathList = new ArrayList<String>();
+        while (sqlRowSet.next()) {
+            filePathList.add(sqlRowSet.getString(1));
+        }
+
+        for (String path : filePathList) {
+            downLoadFile(path,rootpath);
+        }
+    }
+
+    public void downLoadFile(String url,String rootpath ) {
+
+        String fileSavePath;
+        //0.获取文件名称
+        String[] split = url.split("/");
+        String fileName = split[split.length - 1];
+        //1.截取文件路径
+        fileSavePath = url.replaceAll("/" + fileName, "");
+        String subPath = fileSavePath.substring(24);
+        System.out.println(subPath);
+
+        //2.判断该路径是否存在，如果不存在则创建
+        fileSavePath = rootpath + subPath;
+
+        //3.下载文件
+        try {
+            FileDownloadUtil.download(url, fileName, fileSavePath);
+        }catch (Exception e){
+            logger.error("\r\nURL:"+url+"\r\nrootpath:"+rootpath);
+        }
+    }
+}
